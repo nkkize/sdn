6cfb710451276bf065491a1f5de743f210594709
NarenderK <narender.kumar@talentica.com>
2015-10-27 19:17:26 +0530

    file upload

--- src/main/java/com/talentica/sdn/controller/GraphController.java
+++ src/main/java/com/talentica/sdn/controller/GraphController.java
@@ -3,8 +3,16 @@
  */
 package com.talentica.sdn.controller;
 
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
 /**
@@ -14,11 +22,30 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class GraphController {
 
-	@RequestMapping("/welcome")
+	@RequestMapping("/")
 	public ModelAndView greeting() {
 		ModelAndView model = new ModelAndView();
 		model.setViewName("welcome");
 		return model;
 	}
+
+	@RequestMapping(value="/upload", method=RequestMethod.POST)
+    public @ResponseBody String handleFileUpload(@RequestParam("name") String name,
+            @RequestParam("file") MultipartFile file){
+        if (!file.isEmpty()) {
+            try {
+                byte[] bytes = file.getBytes();
+                BufferedOutputStream stream =
+                        new BufferedOutputStream(new FileOutputStream(new File(name)));
+                stream.write(bytes);
+                stream.close();
+                return "You successfully uploaded " + name + "!";
+            } catch (Exception e) {
+                return "You failed to upload " + name + " => " + e.getMessage();
+            }
+        } else {
+            return "You failed to upload " + name + " because the file was empty.";
+        }
+    }
 
 }
